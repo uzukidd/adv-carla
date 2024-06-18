3caf52a868ea78850f9b42c6fd14caefa14c1f9c
uzukidd <uzukidd@gmail.com>
2024-06-18 14:51:50 +0800

    feat: roihead attack added.

---
+++ scripts/attack_scale.sh
@@ -0,0 +1,9 @@
+cd ..
+# rm -rf output/train/scale_ablation/$1_full_attack_$3_$4_$5
+# rm -rf output/train/scale_ablation/$1_iou_frozen_$3_$4_$5
+# rm -rf output/train/scale_ablation/$1_logit_frozen_$3_$4_$5
+rm -rf output/train/scale_ablation/$1_eval_init_patch_$3_$4_$5
+# python attack.py --headbox-attack --exp-name scale_ablation/$1_full_attack_$3_$4_$5 --cfg-file configs/attack_configs/relevant_bounding_box_$1.yaml --device $2
+# python attack.py --headbox-attack --exp-name scale_ablation/$1_iou_frozen_$3_$4_$5 --iou-frozen --cfg-file configs/attack_configs/relevant_bounding_box_$1.yaml --device $2
+# python attack.py --headbox-attack --exp-name scale_ablation/$1_logit_frozen_$3_$4_$5 --logit-frozen --cfg-file configs/attack_configs/relevant_bounding_box_$1.yaml --device $2
+python attack.py --exp-name scale_ablation/$1_eval_init_patch_$3_$4_$5 --eval-init-patch --scale $3 $4 $5 --cfg-file configs/attack_configs/relevant_bounding_box_$1.yaml --device $2
---
+++ scripts/level_ablation.sh
@@ -0,0 +1,4 @@
+bash attack_level.sh pointrcnn 0 0
+bash attack_level.sh pointrcnn 0 1
+bash attack_level.sh pointrcnn 0 3
+bash attack_level.sh pointrcnn 0 4
